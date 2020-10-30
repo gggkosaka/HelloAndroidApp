36f30f05b9a4c0283df0e4ab4b964c83f6351cee
gggkosaka <butakimutii@gmail.com>
2020-10-30 16:19:54 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/genki/kosaka/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.genki.kosaka
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
